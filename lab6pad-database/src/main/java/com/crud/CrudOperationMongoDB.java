69113c08a23c75c8e768aef691db233ebef57ac6
Vovc Artiom <vovc.artiom@gmail.com>
2016-11-12 14:43:57 +0200

    refactoring

---
+++ lab6pad-database/src/main/java/com/crud/CrudOperationMongoDB.java
@@ -0,0 +1,76 @@
+package com.crud;
+
+import com.model.Empl;
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.sun.net.httpserver.HttpExchange;
+import org.bson.BsonDocument;
+import org.bson.BsonString;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CrudOperationMongoDB {
+
+    public void insertOne(Empl empl){
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("lab6pad");
+        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("pad");
+
+        mongoCollection.insertOne(empl.getDocument());
+    }
+
+    public void findOneAndReplace(HttpExchange httpExchange, Empl empl){
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("lab6pad");
+        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("pad");
+
+        mongoCollection.findOneAndReplace(
+                new BsonDocument("firstname", new BsonString(httpExchange.getRequestHeaders().get("firstname").get(0))),
+                empl.getDocument()
+        );
+    }
+
+    public void delete(String firstname){
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("lab6pad");
+        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("pad");
+
+        mongoCollection.deleteOne(
+                new BsonDocument("firstname", new BsonString(firstname))
+        );
+    }
+
+    public List<Empl> find(){
+        List<Empl> empls = new ArrayList<>();
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("lab6pad");
+        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("pad");
+
+        FindIterable<Document> datas = mongoCollection.find();
+        datas.forEach((Block<? super Document>) document -> {
+            Empl empl = new Empl(document);
+            empls.add(empl);
+        });
+        return empls;
+    }
+
+    public List<Empl> findOffsetLimit(Map<String, Integer> values) {
+        List<Empl> empls = new ArrayList<>();
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("lab6pad");
+        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("pad");
+
+        FindIterable<Document> datas = mongoCollection.find().skip(values.get("offset")).limit(values.get("limit"));
+        datas.forEach((Block<? super Document>) document -> {
+            Empl empl = new Empl(document);
+            empls.add(empl);
+        });
+        return empls;
+    }
+}
--- lab6pad-httpserver/src/main/java/com/http/CustomHttpServer.java
+++ lab6pad-httpserver/src/main/java/com/http/CustomHttpServer.java
@@ -1,11 +1,7 @@
 package com.http;
 
+import com.crud.CrudOperationMongoDB;
 import com.model.Empl;
-import com.mongodb.Block;
-import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -13,15 +9,11 @@ import com.sun.net.httpserver.HttpServer;
 import com.util.JSONUtil;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
-import org.bson.BsonDocument;
-import org.bson.BsonString;
-import org.bson.Document;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.StringWriter;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -59,11 +51,7 @@ public class CustomHttpServer {
                 String response = "tuple was inserted";
                 int status = 200;
 
-                MongoClient mongoClient = new MongoClient();
-                MongoDatabase mongoDatabase = mongoClient.getDatabase("lab6pad");
-                MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("pad");
-
-                mongoCollection.insertOne(empl.getDocument());
+                new CrudOperationMongoDB().insertOne(empl);
 
                 httpExchange.sendResponseHeaders(status, response.length());
                 OutputStream os = httpExchange.getResponseBody();
@@ -99,14 +87,7 @@ public class CustomHttpServer {
                 String response = "was updated tuple";
                 int status = 200;
 
-                MongoClient mongoClient = new MongoClient();
-                MongoDatabase mongoDatabase = mongoClient.getDatabase("lab6pad");
-                MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("pad");
-
-                mongoCollection.findOneAndReplace(
-                        new BsonDocument("firstname", new BsonString(httpExchange.getRequestHeaders().get("firstname").get(0))),
-                        empl.getDocument()
-                );
+                new CrudOperationMongoDB().findOneAndReplace(httpExchange, empl);
 
                 httpExchange.sendResponseHeaders(status, response.length());
                 OutputStream os = httpExchange.getResponseBody();
@@ -137,13 +118,7 @@ public class CustomHttpServer {
                 String response = "was deleted tuple";
                 int status = 200;
 
-                MongoClient mongoClient = new MongoClient();
-                MongoDatabase mongoDatabase = mongoClient.getDatabase("lab6pad");
-                MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("pad");
-
-                mongoCollection.deleteOne(
-                        new BsonDocument("firstname", new BsonString(httpExchange.getRequestHeaders().get("firstname").get(0)))
-                );
+                new CrudOperationMongoDB().delete(httpExchange.getRequestHeaders().get("firstname").get(0));
 
                 httpExchange.sendResponseHeaders(status, response.length());
                 OutputStream os = httpExchange.getResponseBody();
@@ -174,16 +149,7 @@ public class CustomHttpServer {
                 Headers h = httpExchange.getResponseHeaders();
                 h.add("Content-Type", "application/json");
 
-                MongoClient mongoClient = new MongoClient();
-                MongoDatabase mongoDatabase = mongoClient.getDatabase("lab6pad");
-                MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("pad");
-
-                FindIterable<Document> datas = mongoCollection.find();
-                List<Empl> empls = new ArrayList<>();
-                datas.forEach((Block<? super Document>) document -> {
-                    Empl empl = new Empl(document);
-                    empls.add(empl);
-                });
+                List<Empl> empls = new CrudOperationMongoDB().find();
 
                 String rns = JSONUtil.getJSONStringfromJAVAObject(empls);
                 httpExchange.sendResponseHeaders(200, rns.length());
@@ -225,16 +191,7 @@ public class CustomHttpServer {
                     values.put(varAndValue[0], Integer.parseInt(varAndValue[1]));
                 }
 
-                MongoClient mongoClient = new MongoClient();
-                MongoDatabase mongoDatabase = mongoClient.getDatabase("lab6pad");
-                MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("pad");
-
-                FindIterable<Document> datas = mongoCollection.find().skip(values.get("offset")).limit(values.get("limit"));
-                List<Empl> empls = new ArrayList<>();
-                datas.forEach((Block<? super Document>) document -> {
-                    Empl empl = new Empl(document);
-                    empls.add(empl);
-                });
+                List<Empl> empls = new CrudOperationMongoDB().findOffsetLimit(values);
 
                 String rns = JSONUtil.getJSONStringfromJAVAObject(empls);
                 httpExchange.sendResponseHeaders(200, rns.length());
